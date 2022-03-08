dfaba7ba59bf479742ea05cf96619b0087848b45
Spencer Carlson <spencerdcarlson@gmail.com>
2022-03-08 09:20:26 -0700

    ship version check [skip ci]

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GoogleCerts.MixProject do
   def project do
     [
       app: :google_certs,
-      version: "1.0.0-alpha",
+      version: "1.0.0",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
